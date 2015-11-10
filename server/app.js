22160c9b6666e4df7f2e9e97641a80795e97c91d
Dave Strus <dave@getfretless.com>
2015-11-10 14:51:53 -0500

    Retrieve notes from database.

--- server/app.js
+++ server/app.js
@@ -1,24 +1,38 @@
 var express = require('express');
 var app = express();
 
+var db = require('mongoose');
+db.connect('mongodb://mongo:ilove1150@ds053198.mongolab.com:53198/notelydb');
+
+var NoteSchema = db.Schema({
+  title: String,
+  body_html: String,
+  body_text: String,
+  updated_at: { type: Date, default: Date.now }
+});
+
+var Note = db.model('Note', NoteSchema);
+
+// Allow CORS
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   next();
 });
 
+// List all notes
 app.get('/notes', function(req, res) {
-  res.json([
-    {
-      title: 'Hardcoded note',
-      body_html: 'This is the body of my hardcoded note.'
-    },
-    {
-      title: 'Another one',
-      body_html: 'w00t'
-    }
-  ]);
+  Note.find().then(function(notes) {
+    res.json(notes);
+  });
 });
 
 app.listen(3000, function() {
   console.log('Listening on http://localhost:3000');
 });
+
+
+
+
+
+
+//
