13f88c4cc6b8fc3ec20d1475eb350283324823d8
Dave Strus <dave@getfretless.com>
2015-11-09 21:38:11 -0500

    Retrieve notes from mongo.

--- server/app.js
+++ server/app.js
@@ -1,5 +1,17 @@
 var express = require('express');
 var app = express();
+var db = require('mongoose');
+
+db.connect('mongodb://mongo:ilove1150@ds051534.mongolab.com:51534/notely');
+
+var NoteSchema = db.Schema({
+  title: String,
+  body_html: String,
+  body_text: String,
+  updated_at: { type: Date, default: Date.now }
+});
+
+var Note = db.model('Note', NoteSchema);
 
 // Cross-Origin Request (CORS) middleware
 app.use(function(req, res, next) {
@@ -7,18 +19,10 @@ app.use(function(req, res, next) {
   next();
 });
 
-
 app.get('/notes', function(req, res) {
-  res.json([
-    {
-      title: 'Hardcoded note!',
-      body_html: 'Cool note.'
-    },
-    {
-      title: 'Super-groovy',
-      body_html: 'Read it and weep.'
-    }
-  ]);
+  Note.find().then(function(notes) {
+    res.json(notes);
+  });
 });
 
 app.listen(3000, function() {
