3e7dd9a2c56f2b0ac46c9c3c2d643d1399f048a1
Dave Strus <dave@getfretless.com>
2015-11-12 14:00:27 -0500

    Save users.

--- client/app/components/sign-up.js
+++ client/app/components/sign-up.js
@@ -1,6 +1,20 @@
 angular.module('notely')
-.directive('signUp', function() {
+.directive('signUp', ['UsersService', (UsersService) => {
+
+  class SignUpController {
+    constructor() {
+      this.user = {};
+    }
+    submit() {
+      UsersService.create(this.user);
+    }
+  }
+
   return {
+    scope: {},
+    controller: SignUpController,
+    controllerAs: 'ctrl',
+    bindToController: true,
     templateUrl: '/components/sign-up.html'
   };
-});
+}]);
